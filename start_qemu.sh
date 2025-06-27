4729a3bd85e6f9aa5f6b63a436cf322c952b1973
nathanrayburn <nathanarmand@hotmail.com>
2025-06-27 22:22:06 +0200

    update

--- start_qemu.sh
+++ start_qemu.sh
@@ -1,13 +1,8 @@
 qemu-system-x86_64 \
-  -machine q35,smm=on \
   -m 2048 \
-  -cpu qemu64 \
   -drive if=pflash,format=raw,readonly=on,file=ovmf/OVMF_CODE.fd \
-  -drive if=pflash,format=raw,file=ovmf/OVMF_VARS.fd \
-  -global driver=cfi.pflash01,property=secure,value=on \
-  -device intel-iommu,intremap=on \
-  -drive id=nvme0,file=fat.img,format=raw,if=none \
-  -device nvme,drive=nvme0,serial=NVME01,bootindex=1 \
+  -drive if=pflash,format=raw,file=Build/OvmfX64/DEBUG_GCC5/FV/OVMF_VARS.fd \
+  -drive file=fat.img,format=raw \
   -s \
   -serial mon:stdio \
   -nographic \
---
+++ start_qemu_nvme.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# QEMU VM with Secure Boot (OVMF), NVMe, and IOMMU enabled
+qemu-system-x86_64 \
+  -machine q35,smm=on \
+  -m 2048 \
+  -cpu qemu64 \
+  -drive if=pflash,format=raw,readonly=on,file=ovmf/OVMF_CODE.fd \
+  -drive if=pflash,format=raw,file=Build/OvmfX64/DEBUG_GCC5/FV/OVMF_VARS.fd \
+  -global driver=cfi.pflash01,property=secure,value=on \
+  -device intel-iommu,intremap=on \
+  -drive id=nvme0,file=fat.img,format=raw,if=none \
+  -device nvme,drive=nvme0,serial=NVME01,bootindex=1 \
+  -s \
+  -serial mon:stdio \
+  -nographic \
+  -debugcon file:debug.log \
+  -global isa-debugcon.iobase=0x402
